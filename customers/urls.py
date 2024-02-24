95be245ce1d0135146976273da1e09a1a466191e
aisha-sali <aishasali1203@gmail.com>
2024-02-24 09:18:55 +0530

    layout design

--- customers/urls.py
+++ customers/urls.py
@@ -20,8 +20,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 urlpatterns = [
-    path('', views.index,name='home'),
-     path('product_list', views.list_products,name='list_product'),
-     path('product_details', views.detail_product,name='detail_product')
+   
+     path('account', views.show_account,name='account'),
+    
 ]
+
+
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('products.urls'))
+    path('',include('products.urls')),
+    path('customer',include('customers.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
